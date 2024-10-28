1096f78b813b2db3710846dc4e1b4708a3396eb0
cambronio99 <owings.cameron@first.edu>
2024-10-28 10:42:28 -0400

    Portfolio Initial Migration
    Migrating Portfolio Site to Git

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle the mobile menu
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.getElementById('nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
